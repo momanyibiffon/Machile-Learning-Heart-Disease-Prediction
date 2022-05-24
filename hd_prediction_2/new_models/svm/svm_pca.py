6e53d80c69bd958c2ca10d4bd98eb380547c3c51
momanyibiffon <momanyibiffon@gmail.com>
2022-05-25 02:44:37 +0800

    first commit

---
+++ hd_prediction_2/new_models/svm/svm_pca.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns
